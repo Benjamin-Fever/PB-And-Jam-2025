9e35751294eafa5797aabbe02721622aeb5f0e88
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-04-27 08:23:40 +1200

    add body type

--- npc/npc.gd
+++ npc/npc.gd
@@ -1,26 +1,36 @@
 class_name NPC
 extends Node2D
 
+
+enum BodyType {
+	SMALL,
+	MEDIUM,
+	LARGE
+}
+
+const BODY_TYPE_OFFSETS = [Vector2(0, 0), Vector2(0, -40), Vector2(0, -75)]
+
 @export var face_color : Color = Color(1, 1, 1)
 @export var has_left_horn: bool = true
 @export var has_right_horn: bool = true
 @export var has_top_fur: bool = true
 @export var has_bottom_fur: bool = true
+@export var current_body_type: BodyType = BodyType.SMALL
 
 
 @export_group("Refrences")
-@export var top_fur : Node2D
-@export var bottom_fur : Node2D
-@export var face_sprite : Sprite2D
-@export var left_horn : Node2D
-@export var right_horn : Node2D
-
+@export var top_fur: Node2D
+@export var bottom_fur: Node2D
+@export var face_sprite: Sprite2D
+@export var left_horn: Node2D
+@export var right_horn: Node2D
+@export var head: Node2D
 
 @onready var burn_animation: AnimationPlayer = $BurnAnimation
 
 
 func _ready():
-	_update();
+	_update()
 
 
 func _update():
@@ -29,6 +39,7 @@ func _update():
 	bottom_fur.visible = has_bottom_fur
 	left_horn.visible = has_left_horn
 	right_horn.visible = has_right_horn
+	head.position = BODY_TYPE_OFFSETS[current_body_type]
 
 func _remove_top_fur():
 	has_top_fur = false
