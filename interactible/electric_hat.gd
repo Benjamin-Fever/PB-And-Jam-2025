d36a1625baba78184ec3836912d25e7953a2cd3e
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-04-27 11:43:21 +1200

    rearrange npc head

--- interactible/electric_hat.gd
+++ interactible/electric_hat.gd
@@ -2,6 +2,9 @@ class_name ElectricHat
 extends ItemRegion
 
 
+var locked_in := false
+
+
 func _ready() -> void:
 	super()
 	process_priority = 10
@@ -11,3 +14,4 @@ func _process(delta: float) -> void:
 	super(delta)
 	if global_position.distance_to(home_position) >= 30.0:
 		global_position = home_position.direction_to(global_position) * 30.0 + home_position
+	
