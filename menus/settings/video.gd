7dc0e6be8778c020d0a89ebd6957be0c6bc7960f
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-04-26 18:37:23 +1200

    Change up settings variable format

--- menus/settings/video.gd
+++ menus/settings/video.gd
@@ -4,19 +4,22 @@ extends ScrollContainer
 
 @export var resolutions: Array[Vector2i] = [Vector2i(854, 480), Vector2i(1280, 720), Vector2i(1920, 1080), Vector2i(2560, 1440), Vector2i(3840, 2160)]
 
+
+@onready var settings_that_dont_work_when_embedded: Array = [
+	$VBoxContainer/Resolution/ResolutionOption, 
+	$VBoxContainer/WindowMode, 
+	$VBoxContainer/HSeparator1, 
+	$VBoxContainer/Resolution, 
+	$VBoxContainer/HSeparator2
+]
+
 @onready var resolution_option: OptionButton = $VBoxContainer/Resolution/ResolutionOption
-@onready var window_mode: Setting = $VBoxContainer/WindowMode
-@onready var h_separator1: HSeparator = $VBoxContainer/HSeparator1
-@onready var resolution: Setting = $VBoxContainer/Resolution
-@onready var h_separator_2: HSeparator = $VBoxContainer/HSeparator2
 
 
 func _ready() -> void:
 	if get_window().is_embedded() || Engine.is_embedded_in_editor():
-		window_mode.visible = false
-		h_separator1.visible = false
-		resolution.visible = false
-		h_separator_2.visible = false
+		for setting in settings_that_dont_work_when_embedded:
+			setting.visible = false
 	
 	resolution_option.clear()
 	for i in range(resolutions.size()):
