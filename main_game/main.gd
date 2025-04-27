de755964570aece9b89be75436a2db75527d6d14
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-04-27 14:58:23 +1200

    start of showing NPC through window

--- main_game/main.gd
+++ main_game/main.gd
@@ -17,13 +17,18 @@ const CHAIR = preload("res://npc/chair.tscn")
 @onready var lever: VoltageRegion = $Interactables/Voltage/Lever
 
 var next_npc: NPC
+var next_chair: Sprite2D
 
 
-func _on_npc_burned_up() -> void:
-	timer_label.reset_timer()
-	
+func _ready() -> void:
+	setup_next_npc()
+
+
+func setup_next_npc() -> void:
+	print("Setup next npc")
 	var new_chair = CHAIR.instantiate()
-	new_chair.position = Vector2(START_X_POS, 540)
+	new_chair.position = Vector2(800, 540)
+	new_chair.z_index = -5
 	add_child(new_chair)
 	
 	var new_npc = NPC_SCENE.instantiate()
@@ -33,22 +38,47 @@ func _on_npc_burned_up() -> void:
 	new_npc.has_right_horn = randf() > 0.5
 	new_npc.current_body_type = randi_range(NPC.BodyType.SMALL, NPC.BodyType.LARGE)
 	new_npc.face_color = Color.from_ok_hsl(0.15, 1.0, randf())
-	new_npc.position = Vector2(START_X_POS, 358)
+	new_npc.position = Vector2(800, 358)
 	new_npc.material = new_npc.material.duplicate(true)
 	new_npc.material.set_shader_parameter("radius", 0.0)
+	new_npc.z_index = -5
 	add_child(new_npc)
 	call_deferred("reset_materials", new_npc)
 	new_npc.connect("burned_up", _on_npc_burned_up)
 	
 	var tween = get_tree().create_tween().set_trans(Tween.TRANS_LINEAR)
 	tween.set_parallel()
+	tween.tween_property(new_npc, "position", Vector2(1600, npc.position.y), 3)
+	tween.tween_property(new_chair, "position", Vector2(1600, chair.position.y), 3)
+	
+	next_chair = new_chair
+	next_npc = new_npc
+
+
+func _on_npc_burned_up() -> void:
+	print("NPC burned up")
+	timer_label.reset_timer()
+	
+	var tween = get_tree().create_tween().set_trans(Tween.TRANS_LINEAR)
+	tween.set_parallel()
+	tween.tween_property(next_npc, "position", Vector2(START_X_POS, next_npc.position.y), 3)
+	tween.tween_property(next_chair, "position", Vector2(START_X_POS, next_chair.position.y), 3)
+	tween.set_parallel(false)
+	tween.tween_property(next_npc, "z_index", 0, 0)
+	tween.tween_property(next_chair, "z_index", 0, 0)
+	tween.set_parallel()
 	tween.tween_property(npc, "position", Vector2(-300, npc.position.y), 3)
 	tween.tween_property(chair, "position", Vector2(-300, chair.position.y), 3)
-	tween.tween_property(new_npc, "position", Vector2(END_X_POS, new_npc.position.y), 3)
-	tween.tween_property(new_chair, "position", Vector2(END_X_POS, new_chair.position.y), 3)
+	tween.tween_property(next_npc, "position", Vector2(END_X_POS, next_npc.position.y), 3)
+	tween.tween_property(next_chair, "position", Vector2(END_X_POS, next_chair.position.y), 3)
 	tween.tween_property(pivot, "rotation", 0, 3)
 	tween.set_parallel(false)
-	tween.tween_callback(destroy_old_npc_chair.bind(new_npc, new_chair))
+	tween.tween_callback(destroy_old_npc_chair)
+	
+	npc = next_npc
+	chair = next_chair
+	lever.connect("start_electrocution", npc._on_lever_start_electrocution)
+	setup_next_npc()
 
 
 func reset_materials(new_npc) -> void:
@@ -57,11 +87,9 @@ func reset_materials(new_npc) -> void:
 	new_npc.body.material = new_npc.material
 
 
-func destroy_old_npc_chair(new_npc, new_chair) -> void:
+func destroy_old_npc_chair() -> void:
 	npc.queue_free()
 	chair.queue_free()
-	npc = new_npc
-	chair = new_chair
 	
 	npc.update_shave_pos()
 	electric_hat.locked_in = false
